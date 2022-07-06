4465943266692bb4df73fc4bb00c59ef2217f601
Anvar Aliyev <aliyeva4381@gmail.com>
2022-07-06 23:32:16 +0500

    BodyParser has been added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const dataRouter = require("./routers/router1");
+
+app.use(bodyParser.json());
 
 app.use("/api/v1/data/", dataRouter);
 
